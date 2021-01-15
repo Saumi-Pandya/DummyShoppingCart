19a92d210ae73185ced02c11ff29ac4e18abdb25
Saumi-Pandya <saumipandya@gmail.com>
2021-01-15 23:19:13 +0530

    Improved ui design

--- app/src/main/java/com/example/dummyshoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/dummyshoppingcart/MainActivity.kt
@@ -7,9 +7,9 @@ import android.os.Bundle
                                  R.drawable.googlealexa, "Help is here. Meet Alexa", "Get entertained all day long and control your smart home with Alexa"),
                           Product(3,"Fitbit",1700F,R.drawable.fitbit,"Health conscious? Try this","Keep a track of the calories burnt without much effort")
                          ,Product(4,"Induction",7500F,R.drawable.induction,"Risk free and efficient","Get rid of heavy gas cylinders" +
-                         "and monthly refilling, it's time to go electric"), Product(5,"Microwave",8700F,R.drawable.microwave,"Save your time",
+                         " and monthly refilling, it's time to go electric"), Product(5,"Microwave",8700F,R.drawable.microwave,"Save your time",
                             "Instantly heats up your food. Also, don't forget to bake cakes with ease anyday, everyday"),Product(6,"Mixer Grinder",
-                           4300F,R.drawable.microwave,"Grind anything, anytime","Get rid of time consuming, boring grinding process, use " +
+                           4300F,R.drawable.mixer,"Grind anything, anytime","Get rid of time consuming, boring grinding process, use " +
                             "electric Mixer Grinder for enhanced performance and efficiency"))
 
 class MainActivity : AppCompatActivity() {
