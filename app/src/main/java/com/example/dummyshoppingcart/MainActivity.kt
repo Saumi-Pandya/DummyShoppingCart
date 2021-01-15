2ab85823d3e460b4998b6d82831ab2c3ff29860a
Saumi-Pandya <saumipandya@gmail.com>
2021-01-15 20:29:48 +0530

    Added items in the list

--- app/src/main/java/com/example/dummyshoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/dummyshoppingcart/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.dummyshoppingcart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
- val products = mutableListOf<Product>(Product(1,"Pixel 3A",65000F, R.drawable.ic_launcher_foreground,"It is a good product",
-                           "Please do buy this. It's great"),Product(2,"Google Alexa",35000F, R.drawable.ic_launcher_background,
-                                "It is a good speaker", "You can instruct it"))
+ val products = mutableListOf<Product>(Product(1,"Pixel 3A",39990F, R.drawable.pixel3a,"Meet Pixel 3A",
+                           "Save your photos with free, unlimited storage with Google Photos."),Product(2,"Google Alexa",7000F,
+                                 R.drawable.googlealexa, "Help is here. Meet Alexa", "Get entertained all day long and control your smart home with Alexa"),
+                          Product(3,"Fitbit",1700F,R.drawable.fitbit,"Health conscious? Try this","Keep a track of the calories burnt without much effort")
+                         ,Product(4,"Induction",7500F,R.drawable.induction,"Risk free and efficient","Get rid of heavy gas cylinders" +
+                         "and monthly refilling, it's time to go electric"), Product(5,"Microwave",8700F,R.drawable.microwave,"Save your time",
+                            "Instantly heats up your food. Also, don't forget to bake cakes with ease anyday, everyday"),Product(6,"Mixer Grinder",
+                           4300F,R.drawable.microwave,"Grind anything, anytime","Get rid of time consuming, boring grinding process, use " +
+                            "electric Mixer Grinder for enhanced performance and efficiency"))
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //val fragmanager = supportFragmentManager.beginTransaction()
-        //fragmanager.add(R)
     }
 }
